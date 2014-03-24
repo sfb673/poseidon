6d982d334afcbb5ae5ab95f94d9bec090c2007c4
Peter Menke <pmenke@googlemail.com>
2014-03-24 15:59:40 +0100

    Removes some unnecessary output, adds license information.

--- lib/poseidon/mixins.rb
+++ lib/poseidon/mixins.rb
@@ -1,6 +1,23 @@
+# This file is part of the POSEIdON gem.
+# Copyright (c) 2012-2014 Peter Menke, SFB 673, Universität Bielefeld
+# http://www.sfb673.org
+#
+# POSEIdON is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# POSEIdON is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with POSEIdON. If not, see
+# <http://www.gnu.org/licenses/>.
 
-
-
+# This module contains all the mixins that can be added to classes.
+# These mixins add RDF export functionality to them.
 module Poseidon
 
   def self.included(base)
